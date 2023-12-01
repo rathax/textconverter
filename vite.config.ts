2a3e16f23f12ebc572e83c27a154b6d488e86c28
drunklol <16104507+patricklol22@users.noreply.github.com>
2023-12-02 00:37:32 +0100

    adding pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,45 @@ import svgLoader from 'vite-svg-loader'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), svgLoader()
+    vue(), svgLoader(),  VitePWA({ 
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Unicode Text Converter',
+        short_name: 'TC',
+        description: 'Generates Fancy Fonts',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+
+      },
+      registerType: 'autoUpdate' })
   ],
   resolve: {
     alias: {
@@ -15,3 +49,4 @@ export default defineConfig({
     }
   }
 })
+
